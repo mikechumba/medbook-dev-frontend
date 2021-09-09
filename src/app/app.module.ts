53c1fdd0cec9c115769c2a8144f6ee676ef1952e
Mike Chumba <michaelchumba09@gmail.com>
2021-09-09 15:49:36 +0300

    create base layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PatientsComponent } from './patients/patients.component';
+import { RegisterPatientComponent } from './register-patient/register-patient.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatientsComponent,
+    RegisterPatientComponent
   ],
   imports: [
     BrowserModule,
